9cbb7b766038be53b9a5e23496f6d11ed2103c00
mohit singh <mohit.singh@polestarllp.com>
2020-09-24 19:52:57 +0530

    Updates

---
+++ precache-manifest.1b1696a0b3aaafd03ffd8580af034962.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "/react-native/./fonts/Foundation.ttf"
+  },
+  {
+    "revision": "42189f2acd820e9f6fcc",
+    "url": "/react-native/static/js/app.854cd369.chunk.js"
+  },
+  {
+    "revision": "1f8c55ebb0ad070cdcd6",
+    "url": "/react-native/static/js/runtime~app.2826e25f.js"
+  },
+  {
+    "revision": "3c851d60ad5ef3f2fe43ebd263490d78",
+    "url": "/react-native/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "f3a20bc023672b9adee3007e37662bae",
+    "url": "/react-native/register-service-worker.js"
+  },
+  {
+    "revision": "d0c694b562b2208635f250762cd7fc79",
+    "url": "/react-native/serve.json"
+  },
+  {
+    "revision": "5bfcb0bea2bc78aec857",
+    "url": "/react-native/static/js/2.b89ce3fe.chunk.js"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/react-native/./fonts/Entypo.ttf"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/react-native/./fonts/MaterialIcons.ttf"
+  },
+  {
+    "revision": "721904d0adef28c6544e02409092acc4",
+    "url": "/react-native/manifest.json"
+  },
+  {
+    "revision": "1826ab3215ea70e37481cd30dc7f590c",
+    "url": "/react-native/index.html"
+  },
+  {
+    "revision": "5e52768b0f7cbb62cbe07751add33101",
+    "url": "/react-native/favicon.ico"
+  },
+  {
+    "revision": "3d85359b37ca7590f8e91d593d02ddaf",
+    "url": "/react-native/favicon-32.png"
+  },
+  {
+    "revision": "afd5733ac789ab1795f1ed7a26e1ecd3",
+    "url": "/react-native/favicon-16.png"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/react-native/./fonts/SimpleLineIcons.ttf"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/react-native/expo-service-worker.js"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/react-native/./fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "b49ae8ab2dbccb02c4d11caaacf09eab",
+    "url": "/react-native/./fonts/Fontisto.ttf"
+  },
+  {
+    "revision": "b70cea0339374107969eb53e5b1f603f",
+    "url": "/react-native/./fonts/FontAwesome5_Solid.ttf"
+  },
+  {
+    "revision": "c39278f7abfc798a241551194f55e29f",
+    "url": "/react-native/./fonts/FontAwesome5_Brands.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-native/./fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "ca9ce9ff0676a9b04ef0f8a3ad17dd08",
+    "url": "/react-native/./fonts/Feather.ttf"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "/react-native/./fonts/AntDesign.ttf"
+  }
+];
--- register-service-worker.js
+++ register-service-worker.js
@@ -3,7 +3,7 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
     navigator.serviceWorker
-      .register('/expo-service-worker.js', { scope: '/' })
+      .register('/react-native/expo-service-worker.js', { scope: '/react-native/' })
       .then(function(info) {
         // console.info('Registered service-worker', info);
       })
